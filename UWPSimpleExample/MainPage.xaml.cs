4d3b66ba137204d30c23bc51ec85676ed135575b
Patrik Simic <patrik.simic@gmail.com>
2023-03-27 18:50:27 +0200

    init

---
+++ UWPSimpleExample/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using PSPDFKit.Document;
+using PSPDFKit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using PSPDFKit.Pdf;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPSimpleExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            PdfView.InitializationCompletedHandler += OnPdfViewInitializationCompleted;
+        }
+
+        private void OnPdfViewInitializationCompleted(PdfView sender, Document doc)
+        {
+            Button_OpenPDF.IsEnabled = true;
+        }
+
+        private async void Button_OpenPDF_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            picker.FileTypeFilter.Add(".pdf");
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var document = DocumentSource.CreateFromStorageFile(file);
+                await PdfView.Controller.ShowDocumentAsync(document);
+            }
+        }
+    }
+}
